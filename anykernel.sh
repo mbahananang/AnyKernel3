8be906d5d0c7b2b83c0b1cc4ef26f5a5fd13049a
iConnanT <88953839+mbahananang@users.noreply.github.com>
2024-03-14 19:02:21 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Silver Wolf kernel by Kyozune
+kernel.string=Fanxing kernel by iConnanT
 do.devicecheck=1
 no_magisk_check=1
 do.modules=0
